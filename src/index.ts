47a034e0a68746bd41e74dbc0c8858d58ac0d678
onion chunky <78101139+ChunkyProgrammer@users.noreply.github.com>
2022-05-18 16:04:29 -0400

    Add privacy related search providers

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { Suggestion } from './providers/base';
+import { Brave } from './providers/Brave';
 import { DuckDuckGo } from './providers/DuckDuckGo';
 import { Google } from './providers/Google';
+import { Qwant } from './providers/Qwant';
+import { Startpage } from './providers/Startpage';
+import { SwisCows } from './providers/SwissCows';
 import { Yahoo } from './providers/Yahoo';
 
-export type SearchProviderType = 'Google' | 'Yahoo' | 'DuckDuckGo' | 'random';
+export type SearchProviderType = 'Brave' | 'DuckDuckGo' | 'Google' | 'Qwant' | 'Startpage' | 'SwissCows' | 'Yahoo' | 'random';
 
 /**
  * Gets search suggestions for a partial search
@@ -17,13 +21,21 @@ export async function getSuggestions(partialSearch: string, searchProvider: Sear
   let provider = searchProvider;
 
   if (provider === 'random') {
-    const providers = ['Google', 'Yahoo', 'DuckDuckGo'];
+    const providers = ['Brave', 'DuckDuckGo', 'Google', 'Qwant', 'Startpage', 'SwissCows', 'Yahoo'];
     provider = providers[Math.floor(Math.random() * providers.length)] as SearchProviderType;
   }
 
   switch (provider) {
+    case 'Brave':
+      return Brave.getSuggestions(partialSearch);
     case 'DuckDuckGo':
       return DuckDuckGo.getSuggestions(partialSearch);
+    case 'Qwant':
+      return Qwant.getSuggestions(partialSearch);
+    case 'Startpage':
+      return Startpage.getSuggestions(partialSearch);
+    case 'SwissCows':
+      return SwisCows.getSuggestions(partialSearch);
     case 'Yahoo':
       return Yahoo.getSuggestions(partialSearch);
     default:
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -14,6 +14,20 @@ describe('Test getting Google suggestions', () => {
   });
 });
 
+describe('Test getting Brave suggestions', () => {
+  it('Should return suggestions for common search terms', async () => {
+    const suggestions = await getSuggestions('hotels', 'Brave');
+
+    expect(suggestions.length).toBeGreaterThan(0);
+  });
+
+  it('Should should not return suggestions for weird search terms', async () => {
+    const suggestions = await getSuggestions('saduqlkjasdku,ylkajsdkhasd', 'Brave');
+
+    expect(suggestions.length).toBe(0);
+  });
+});
+
 describe('Test getting DuckDuckGo suggestions', () => {
   it('Should return suggestions for common search terms', async () => {
     const suggestions = await getSuggestions('hotels', 'DuckDuckGo');
@@ -23,6 +37,48 @@ describe('Test getting DuckDuckGo suggestions', () => {
 
   it('Should should not return suggestions for weird search terms', async () => {
     const suggestions = await getSuggestions('saduqlkjasdku,ylkajsdkhasd', 'DuckDuckGo');
+
+    expect(suggestions.length).toBe(0);
+  });
+});
+
+describe('Test getting Qwant suggestions', () => {
+  it('Should return suggestions for common search terms', async () => {
+    const suggestions = await getSuggestions('hotels', 'Qwant');
+
+    expect(suggestions.length).toBeGreaterThan(0);
+  });
+
+  it('Should should not return suggestions for weird search terms', async () => {
+    const suggestions = await getSuggestions('saduqlkjasdku,ylkajsdkhasd', 'Qwant');
+
+    expect(suggestions.length).toBe(0);
+  });
+});
+
+describe('Test getting Startpage suggestions', () => {
+  it('Should return suggestions for common search terms', async () => {
+    const suggestions = await getSuggestions('hotels', 'Startpage');
+
+    expect(suggestions.length).toBeGreaterThan(0);
+  });
+
+  it('Should should not return suggestions for weird search terms', async () => {
+    const suggestions = await getSuggestions('saduqlkjasdku,ylkajsdkhasd', 'Startpage');
+
+    expect(suggestions.length).toBe(0);
+  });
+});
+
+describe('Test getting SwissCows suggestions', () => {
+  it('Should return suggestions for common search terms', async () => {
+    const suggestions = await getSuggestions('hotels', 'SwissCows');
+
+    expect(suggestions.length).toBeGreaterThan(0);
+  });
+
+  it('Should should not return suggestions for weird search terms', async () => {
+    const suggestions = await getSuggestions('saduqlkjasdku,ylkajsdkhasd', 'SwissCows');
 
     expect(suggestions.length).toBe(0);
   });
@@ -55,3 +111,4 @@ describe('Test getting random suggestions', () => {
     expect(suggestions.length).toBe(0);
   });
 });
+
